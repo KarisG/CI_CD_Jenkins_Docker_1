027aa9101f7ce193ef3596e3ab5ea41f5e4d9a71
Karis <75758274+KarisG@users.noreply.github.com>
2023-02-05 13:37:31 +0100

    Update api.py

--- api.py
+++ api.py
@@ -7,8 +7,8 @@ class_names = ['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt
 @app.route('/classify', methods=['POST'])
 def predict():
     input_data = request.get_json()
-    
     to_predict = np.array(input_data["data"]).reshape(-1, 28, 28, 1)
+    
     model = keras.models.load_model('TP7MLOPS.h5')
 
     prediction = model.predict(to_predict)
